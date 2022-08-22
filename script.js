b2e51af1d97e83d0c71ea85eee171f69b7bfa5d9
SmiriMAziz <mohamedaziz.smiri@etudiant-isi.utm.tn>
2022-08-22 21:08:51 +0100

    phase3

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const courseTemplate = document.querySelector("[template-container]")
-const userCardContainer = document.querySelector("[courses-names]")
+const courseTemplateone = document.querySelector("[template-containerone]")
+const userCardContainer = document.querySelector("[all-courses]")
+const userCardContainerone = document.querySelector("[courses-names]")
 const searchInput = document.querySelector("[data-search]")
+console.log(userCardContainer) ; 
 
+const fields = ["python" , "excel" , "web" , "js" , "data" , "aws" , "draw"] ; 
 let courses = []
 
 searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase()
-  console.log(value)
+  
   courses.forEach(course => {
     const isVisible =
       course.header.toLowerCase().includes(value) ||
@@ -15,19 +19,55 @@ searchInput.addEventListener("input", e => {
   })
 })
 
-fetch("db.json")
+
+function change_to(field){
+  
+  userCardContainer.innerHTML = "" ; 
+  userCardContainerone.innerHTML = "" ; 
+  let t = "active" ; 
+  course = [] ; 
+  fetch(`${field}.json`)
   .then(res => res.json())
   .then(data => {
-    courses = data.coursees.map(course => {
-      const templatecourse = courseTemplate.content.cloneNode(true).children[0]
-      templatecourse.querySelector("[course-image]").src = course.img
-      templatecourse.querySelector("[course-header]").textContent = course.header
-      templatecourse.querySelector("[course-paragraph]").textContent = course.description
-      templatecourse.querySelector("[course-note]").textContent = course.rating
-      templatecourse.querySelector("[course-students]").textContent = course.students
-      templatecourse.querySelector("[price-div]").textContent = course.price
-      userCardContainer.append(templatecourse)
-      return {header:course.header , description:course.description ,element:templatecourse}
-    })
-    console.log(courses)
+    const templatecourseone = courseTemplateone.content.cloneNode(true).children[0]
+    templatecourseone.querySelector("[header-text-container]").textContent = data.head.content ; 
+    templatecourseone.querySelector("[par-text-container]").textContent = data.desc.content ; 
+    templatecourseone.querySelector("[explore-python-container]").textContent = data.btn.content ; 
+    userCardContainerone.appendChild(templatecourseone) ; 
+      courses = data.coursees.map(course => {
+
+        const templatecourse = courseTemplate.content.cloneNode(true).children[0]
+        console.log(course) ; 
+        console.log(templatecourse) ; 
+        if(t!=""){
+          templatecourse.querySelector("[course-title]").classList.add(t) ; 
+           t = "" ; 
+        }
+        templatecourse.querySelector("[course-image]").src = course.img
+        templatecourse.querySelector("[course-header]").textContent = course.header
+        templatecourse.querySelector("[course-paragraph]").textContent = course.description
+        templatecourse.querySelector("[course-note]").textContent = course.rating
+        templatecourse.querySelector("[course-students]").textContent = course.students
+        templatecourse.querySelector("[price-div]").textContent = course.price
+        
+        
+        userCardContainer.append(templatecourse)
+        
+      
+      
+        return {header:course.header , description:course.description ,element:templatecourse}
+      })
+    
   })
+}
+
+change_to("python") ; 
+
+fields.forEach(field =>{
+  const clik = document.getElementById(field) ; 
+  clik.addEventListener("click" , () => {
+    
+
+    change_to(field) ; 
+  })
+})
